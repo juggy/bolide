a19bd4a2adb27a718ab6d711f3df550f44817250
Julien Guimont <juggy@booky.local>
2010-02-23 23:32:27 -0500

    Deployment #2

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -1,8 +1,8 @@
 namespace :deploy do
   task :restart do
     run [
-      "cp #{current_path}/server/nginx/ws.jguimont.com /etc/nginx/sites-enabled/ws.jguimont.com",
-      "cp #{current_path}/server/nginx/live.jguimont.com /etc/nginx/sites-enabled/live.jguimont.com",
+      "cp #{current_path}/config/server/nginx/ws.jguimont.com /etc/nginx/sites-enabled/ws.jguimont.com",
+      "cp #{current_path}/config/server/nginx/live.jguimont.com /etc/nginx/sites-enabled/live.jguimont.com",
       "/etc/init.d/nginx restart"
       ].join(" && ")
   end
