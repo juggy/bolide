34ddad588cb4aab363f54da0f79b161340c08902
Julien Guimont <juggy@booky.local>
2010-02-24 19:18:29 -0500

    Deployment #9

--- config/recipes/apps.rb
+++ config/recipes/apps.rb
@@ -5,9 +5,11 @@ namespace :deploy do
   task :bundle_update do
     run [
       "cd #{current_path}/ws_app",
-      "/var/lib/gems/1.8/bin/bundle install",
+      "sudo bundle lock",
+      "sudo bundle install",
       "cd #{current_path}/stream_app",
-      "/var/lib/gems/1.8/bin/bundle install"
+      "sudo bundle lock",
+      "sudo bundle install"
       ].join(" && ")
   end
   
@@ -22,9 +24,9 @@ namespace :deploy do
   task :start do
     run [
          "cd #{current_path}/stream_app",
-        "/var/lib/gems/1.8/bin/rainbows -c #{current_path}/config/server/app/live.jguimont.com.rb -E production -D",
+        "sudo rainbows -c #{current_path}/config/server/app/live.jguimont.com.rb -E production -D",
         "cd #{current_path}/ws_app",
-        "/var/lib/gems/1.8/bin/unicorn_rails -c #{current_path}/config/server/app/ws.jguimont.com.rb -E production -D"
+        "sudo unicorn_rails -c #{current_path}/config/server/app/ws.jguimont.com.rb -E production -D"
     ].join(" && ")
   end
   
