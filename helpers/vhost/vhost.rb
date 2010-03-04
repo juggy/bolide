0927acfb5c7b92bfe99d04d724133a989b786a31
Julien Guimont <juggy@booky.local>
2010-03-03 20:51:03 -0500

    solidify vhost helper

--- helpers/vhost/vhost.rb
+++ helpers/vhost/vhost.rb
@@ -13,39 +13,43 @@ Bundler.require(:default)
 require 'mq'
 require 'yaml'
 require 'daemons'
-
+require 'logger'
 require File.expand_path("../../../lib/config/amqp_connection", __FILE__)
+
+logger = Logger.new(File.expand_path("../log/vhost.log", __FILE__), 12, 'monthly')
 
 ENV['RAILS_ENV'] = 'production'
 
-#write pid to a file
-#File.open(File.expand_path("../tmp/pids/vhost.pid", __FILE__), "w") do |f|
-#  f.write(Process.pid)
-#end
 Daemons.run_proc('vhost') do
-  
-  AMQP.start(AmqpConnection::connection.merge!({:vhost=>'/bolide'})) do
+while true 
+  begin
+    AMQP.start(AmqpConnection::connection.merge!({:vhost=>'/bolide'})) do
  
-    q = MQ.queue('vhost')
+      q = MQ.queue('vhost')
   
-    q.pop do |msg| 
-      unless msg
-        EM.add_timer(5){ q.pop }
-      else
-        msg_obj = YAML::load( msg );
-        if msg_obj.has_key?("add_vhost")
-          system ("rabbitmqctl add_vhost " + msg_obj["add_vhost"])
-          system ("rabbitmqctl set_permissions -p " + msg_obj["add_vhost"] + ' guest \'.*\' \'.*\' \'.*\'')
-        end
+      q.pop do |msg| 
+        unless msg
+          EM.add_timer(5){ q.pop }
+        else
+          msg_obj = YAML::load( msg );
+          if msg_obj.has_key?("add_vhost")
+            system ("rabbitmqctl add_vhost " + msg_obj["add_vhost"])
+            system ("rabbitmqctl set_permissions -p " + msg_obj["add_vhost"] + ' guest \'.*\' \'.*\' \'.*\'')
+          end
       
-        if msg_obj.has_key?("delete_vhost")
-          system ("rabbitmqctl delete_vhost " + msg_obj["delete_vhost"])
-        end
+          if msg_obj.has_key?("delete_vhost")
+            system ("rabbitmqctl delete_vhost " + msg_obj["delete_vhost"])
+          end
       
-        #directly check new entries
-        q.pop
+          #directly check new entries
+          q.pop
+        end
       end
-    end
   
+    end
+  rescue Exception => e
+    #something went wrong
+    logger.error "Vhost Error: #{e}"
   end
+end
 end
