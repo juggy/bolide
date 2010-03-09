f37de0907ebd1fe32ae75f1383d3ef13a1cfcd21
Julien Guimont <juggy@booky.local>
2010-03-09 01:36:12 -0500

    email

--- feature_app/config/initializers/email.rb
+++ feature_app/config/initializers/email.rb
@@ -3,3 +3,4 @@ if RAILS_ENV != 'test'
   email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
   ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV] unless email_settings[RAILS_ENV].nil?
 end
+p ActionMailer::Base.smtp_settings
