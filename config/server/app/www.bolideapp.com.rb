26e0f9fa3f4cee25ddc26695aaef74ecab024001
Julien Guimont <juggy@booky.local>
2010-03-10 00:05:20 -0500

    changed from unicorn_rails to unicorn

--- config/server/app/www.bolideapp.com.rb
+++ config/server/app/www.bolideapp.com.rb
@@ -17,6 +17,7 @@ timeout 10
 if rails_env == 'production'
   working_directory "/u/apps/bolide/current/feature_app"
   logger Logger.new("/u/apps/bolide/current/feature_app/log/unicorn.www.log", 'monthly')
+  pid '/u/apps/bolide/current/feature_app/tmp/pids/unicorn.pid'
   listen '/u/apps/bolide/current/feature_app/tmp/sockets/www.sock', :backlog => 2048
 end
  
